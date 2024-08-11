d5b8c3b6396707db1ce9a22b6b75358f116e7aae
aldifm22 <aldifauzanmawardi22122000@gmail.com>
2024-08-11 10:40:02 +0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Button from './Button'
-import VideoBg from '@/videos/ARTEMAOK.mp4'
+
 const Hero = () => {
   return (
     <section className="max-container padding-container flex items-center flex-col gap-10 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row  bg-texture  ">
